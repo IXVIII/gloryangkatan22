e0e5c30e4aa98f427f4e1a4f4401a980582f3b9f
rafiakbarrr <110707777+rafiakbarrr@users.noreply.github.com>
2025-07-28 11:56:22 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,14 +1,24 @@
 import { next } from '@vercel/edge';
+import { NextResponse } from 'next/server';
 
 export default function middleware(req) {
-  return next({
-    headers: {
-      'Referrer-Policy': 'origin-when-cross-origin',
-      'X-Frame-Options': 'DENY',
-      'X-Content-Type-Options': 'nosniff',
-      'X-DNS-Prefetch-Control': 'on',
-      'Strict-Transport-Security':
-        'max-age=31536000; includeSubDomains; preload',
-    },
-  });
+  const url = req.nextUrl.clone();
+  const token = req.cookies.get('auth_token')
+
+  // Jika belum login dan mencoba akses /
+  if (!token && url.pathname === '/') {
+    url.pathname = '/index';
+    return NextResponse.redirect(url);
+  }else{
+      return next({
+      headers: {
+        'Referrer-Policy': 'origin-when-cross-origin',
+        'X-Frame-Options': 'DENY',
+        'X-Content-Type-Options': 'nosniff',
+        'X-DNS-Prefetch-Control': 'on',
+        'Strict-Transport-Security':
+          'max-age=31536000; includeSubDomains; preload',
+      },
+    });
+  }
 }
