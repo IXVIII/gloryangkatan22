ef0fcb329347ff2e25b143a582f25e5f9f3936a4
Fahriel Dwifaldi <121069396+IXVIII@users.noreply.github.com>
2025-07-29 13:14:03 +0700

    localStorage_isLoggedIn.js

--- middleware.js
+++ middleware.js
@@ -4,9 +4,13 @@ import { next } from '@vercel/edge';
 export function middleware(req) {
   const url = req.nextUrl.clone();
   const token = req.cookies.get('auth_token');
-
+  const isLoggedIn = localStorage.getItem('isLoggedIn');
+  if (!isLoggedIn) {
+      // Redirect ke halaman review jika belum login
+      window.location.href = '/index.html';
+  }
   // Jika belum login dan mencoba akses /
-  if (!token && url.pathname === '/') {
+  if (!token && url.pathname === '/'){
     url.pathname = '/review';
     return NextResponse.redirect(url);
   }
